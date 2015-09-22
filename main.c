9efced91bfff71ee0ab665855b839d6d45eebcbe
Oscar Javier Hinojosa Luna <oscar_jo@hotmail.com>
2015-09-22 08:41:58 -0500

    Agrege el main

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Oscar Javier
+ *
+ * Created on 22 de septiembre de 2015, 08:11 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);s
+}
+
